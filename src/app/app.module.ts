5dc4f36a105181a4c392529aae57606761decb76
xtian7 <xtian7@live.fr>
2018-07-05 01:58:48 +0200

    config angular-cli universal
    angular-cli 1.7.4 + angular universal

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule.withServerTransition({ appId: 'cli-uni-web-met' })],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
